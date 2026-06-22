88b8bc07695f91ca5633b6226fd39133c46e8d73
wadjih <wadjih@local>
2026-06-22 23:54:55 +0100

    fix: rename greetd.tuigreet to tuigreet

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -4,18 +4,16 @@
   programs.hyprland.enable = true;
   programs.hyprland.xwayland.enable = true;
 
-  # greetd + tuigreet: أخف display manager
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland";
         user = "greeter";
       };
     };
   };
 
-  # أوقف GNOME وGDM
   services.displayManager.gdm.enable = false;
   services.desktopManager.gnome.enable = false;
   services.xserver.enable = false;
